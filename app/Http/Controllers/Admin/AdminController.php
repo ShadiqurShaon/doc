e514284ef5bab82938451fdd7c4a493ba1815c50
shadiqurshaon <shadiqurshaon@gmail.com>
2020-04-09 09:40:48 +0600

    adding the index of admin

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -15,6 +15,26 @@ class AdminController extends Controller
     public function index()
     {
         
+        $firestore = app('firebase.firestore');
+   		$database = $firestore->database();
+   		$docRefe = $database->collection('doctors');
+   		$patientRefe = $database->collection('patient');
+   		$doctors = $docRefe->documents();
+   		$patients = $patientRefe->documents();
+
+   		$doctor_count = 0;
+   		foreach ($doctors as $key => $value) {
+   			$doctor_count++;
+   		}
+
+
+   		$patient = $patientRefe->documents();
+   		$patient_count=0;
+   		foreach ($patients as $key => $value) {
+   			$patient_count++;
+   		}
+
+   		
         return view('admin.index');
         
     }
